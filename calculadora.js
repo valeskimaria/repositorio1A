7e8a72b90a5dca88da724536490b3bd259f9a6a5
valeskimaria <145721301+valeskimaria@users.noreply.github.com>
2023-09-28 20:04:24 -0300

    Create calculadora.js

---
+++ calculadora.js
@@ -0,0 +1,5 @@
+function somar(){
+  var valor1 = parseInt (document.getElementById("num1").value, 10);
+  var valor2 = parseInt (document.getElementById("num2").value, 10);
+  document.getElementById("resultado").value= valor1 + valor2;
+}
